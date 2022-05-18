21fc12b369ec1ba2f0bd6fd3e605e18d77dab1dc
FatehKhalsa <manjodhchahal@Manjodhs-MacBook-Pro.local>
2022-05-18 10:12:42 +0100

    Code Refactor

--- src/components/asthans/FresnoCA.js
+++ src/components/asthans/FresnoCA.js
@@ -1,15 +1,24 @@
 import React, {useState} from 'react';
 import { useHistory } from "react-router-dom";
 import MTable from '../helper/materialTable';
+import {hostColumns, hostMockdata} from '../mockData/users'
+
 
 
 const FresnoCA = () =>{
     const history = useHistory();
 
+    const [rowData] = useState(hostMockdata);
+
+    const [columnDefs] = useState(hostColumns);
+
     return (
         <>
         <h3 style={{marginLeft: '42%'}}>Fresno USA Asthan</h3>  
         <button className="btn btn-primary" onClick={() => history.goBack()}>Back</button>
+        <div style={{marginTop: "2%"}}>
+         <MTable rowData={rowData} columnDefs={columnDefs} text={"User"}/>
+         </div>
         </>
     )
 }
--- src/components/asthans/WalsallUK.js
+++ src/components/asthans/WalsallUK.js
@@ -1,16 +1,25 @@
 import React, {useState} from 'react';
 import { useHistory } from "react-router-dom";
 import MTable from '../helper/materialTable';
+import {hostColumns, hostMockdata} from '../mockData/users'
+
 
 
 
 const WalsallUK = () =>{
     const history = useHistory();
 
+    const [rowData] = useState(hostMockdata);
+
+    const [columnDefs] = useState(hostColumns);
+
     return (
        <>
        <h3 style={{marginLeft: '42%'}}>Walsall UK Asthan</h3>  
         <button className="btn btn-primary" onClick={() => history.goBack()}>Back</button>
+        <div style={{marginTop: "2%"}}>
+         <MTable rowData={rowData} columnDefs={columnDefs} text={"User"}/>
+         </div>
         </>
     )
 }
