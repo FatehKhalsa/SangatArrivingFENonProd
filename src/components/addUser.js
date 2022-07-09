afb40138da46a9b285fc22a489dd01b2269c629d
FatehKhalsa <manjodhchahal@Manjodhs-MBP.attlocal.net>
2022-07-08 19:34:58 -0700

    Added input validation on form

--- src/components/addUser.js
+++ src/components/addUser.js
@@ -21,20 +21,25 @@ const AddNewUser = (props) => {
   const [loading, setLoading] = useState(false);
   const [sangatValue, setSangatValue] = useState({
     user_firstName:"",
+    user_middleName: "",
     user_lastName:"",
     user_yearOfBirth:"",
     user_gender:"",
     user_city:"",
+    user_state: "",
     user_country:"",
     user_allergy:"",
     user_hasAllergy:false,
     user_phoneNumber:"",
+    user_email: "",
     user_arrivingFlight: "",
     user_arrivingAirport:"",
     user_departingFlight: "",
     user_departingAirport: "",
     user_hostedby:"",
-    user_goingToAsthan: ""
+    user_goingToAsthan: "",
+    user_emergencyContact: "",
+    user_comments: "",
   });
 
   const handleClose = () => {
@@ -42,6 +47,7 @@ const AddNewUser = (props) => {
     setShowDialong(true);
     setShow(false);
   }
+
   const handleShow = () => setShow(true);
 
   const addNewSangat = async() => {
@@ -71,6 +77,11 @@ const AddNewUser = (props) => {
 const setSangatFirstName = (e) => {
   e.preventDefault();
   setSangatValue({...sangatValue, user_firstName: e.target.value});
+}
+
+const setSangatMiddletName = (e) => {
+  e.preventDefault();
+  setSangatValue({...sangatValue, user_middleName: e.target.value});
 }
 
 const setSangatLastName = (e) => {
@@ -131,6 +142,11 @@ const setSangatPhoneNumber = (e) => {
   setSangatValue({...sangatValue, user_phoneNumber: e.target.value});
 }
 
+const setSangatEmail = (e) => {
+  e.preventDefault();
+  setSangatValue({...sangatValue, user_email: e.target.value});
+}
+
 const setSangatHostedBy = (e) => {
   e.preventDefault();
   setSangatValue({...sangatValue, user_hostedby: e.target.value});
@@ -141,10 +157,19 @@ const setSangatAsthan = (e) => {
   setSangatValue({...sangatValue, user_goingToAsthan: e.target.value});
 }
 
+const setSangatEmergencyContact = (e) => {
+  e.preventDefault();
+  setSangatValue({...sangatValue, user_emergencyContact: e.target.value});
+}
+
+const setSangatComments = (e) => {
+  e.preventDefault();
+  setSangatValue({...sangatValue, user_comments: e.target.value});
+}
+
 //document.getElementsByClassName('addSangat').addEventListener('click', handleShow)
 const dselect = document.querySelectorAll('.addSangat');
 dselect.forEach(el => el.addEventListener('click', handleShow));
-
 
   return (
     <>
@@ -158,20 +183,26 @@ dselect.forEach(el => el.addEventListener('click', handleShow));
         </Modal.Header>
         <Modal.Body style={{ backgroundColor: '#f2f2f2' }}>
           <div style={{ ...editStyling }}>
-            First Name
-            <input style={{ ...inputStyle }} required={true} value ={sangatValue.user_firstName} onChange ={e=>setSangatFirstName(e)}/>
-            Last Name
+            First Name * 
+            <input style={{ ...inputStyle }}  value ={sangatValue.user_firstName} onChange ={e=>setSangatFirstName(e)}/>
+            Middle Name
+            <input style={{ ...inputStyle }} value ={sangatValue.user_middleName} onChange ={e=>setSangatMiddletName(e)}/>
+            Last Name *
             <input style={{ ...inputStyle }}  value ={sangatValue.user_lastName} onChange ={e=>setSangatLastName(e)}/>
             Gender
             <input style={{ ...inputStyle }}  value ={sangatValue.user_gender} onChange ={e=>setSangatGender(e)}/>
-            DOB
+            DOB *
             <input type="date" style={{ ...inputStyle }} value={sangatValue.user_yearOfBirth} onChange ={e=>setSangatYearOfBirth(e)} />
-            City
+            City * 
             <input style={{ ...inputStyle }}  value ={sangatValue.user_city} onChange ={e=>setSangatCity(e)}/>
-            Country
+            State/Province * 
+            <input style={{ ...inputStyle }}  value ={sangatValue.user_city} onChange ={e=>setSangatCity(e)}/>
+            Country * 
             <input style={{ ...inputStyle }}  value ={sangatValue.user_country} onChange ={e=>setSangatCountry(e)}/>
-            Phone Number (Whatsapp)
+            Phone Number (Whatsapp) * 
             <input style={{ ...inputStyle }}  value ={sangatValue.user_phoneNumber} onChange ={e=>setSangatPhoneNumber(e)}/>
+            Email * 
+            <input style={{ ...inputStyle }}  value ={sangatValue.user_email} onChange ={e=>setSangatEmail(e)}/>
             Allergies
             <input style={{ ...inputStyle }}  value ={sangatValue.user_allergy} onChange ={e=>setSangatAllergy(e)}/>
             Arriving Flight Info
@@ -184,6 +215,10 @@ dselect.forEach(el => el.addEventListener('click', handleShow));
             <input style={{ ...inputStyle }} value={sangatValue.user_departingAirport} onChange = {e=>setSangatDepartingAirport(e)} />
             Visiting Asthan
             <input style={{ ...inputStyle }} value={sangatValue.user_goingToAsthan} onChange = {e=>setSangatAsthan(e)} /> 
+            Emergency Contact
+            <input style={{ ...inputStyle }} value={sangatValue.user_emergencyContact} onChange = {e=>setSangatEmergencyContact(e)} /> 
+            Comments
+            <input style={{ ...inputStyle }} value={sangatValue.user_comments} onChange = {e=>setSangatComments(e)} /> 
           </div>
 
         </Modal.Body>
@@ -191,7 +226,7 @@ dselect.forEach(el => el.addEventListener('click', handleShow));
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={addNewSangat}>
+          <Button variant="primary" onClick={addNewSangat} disabled={sangatValue.user_firstName==="" || sangatValue.user_lastName==="" || sangatValue.user_yearOfBirth==="" || sangatValue.user_email==="" || sangatValue.user_phoneNumber===""}>
             Save Changes
           </Button>
         </Modal.Footer>
