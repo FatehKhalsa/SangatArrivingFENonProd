4f59374b3e08f74bbab20052d7c60400177473e3
FatehKhalsa <manjodhchahal@Manjodhs-MBP.attlocal.net>
2022-10-06 09:02:12 -0700

    Changed API mapping to non prod

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
-export const HerokuURL = 'https://guarded-gorge-38921.herokuapp.com/';
-// export const HerokuURL = 'http://localhost:8080/';
+//export const HerokuURL = 'https://guarded-gorge-38921.herokuapp.com/';
+export const HerokuURL = 'https://sangat-arrival-backend-nonprod.herokuapp.com/';
+
 
 export const LOCAL_DATE_FORMAT = "YYYY-MM-DD";
 
