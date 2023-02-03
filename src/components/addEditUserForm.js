105d078ec03d973c8362af5fa05defa162d36697
FatehKhalsa <manjodhchahal@Manjodhs-MacBook-Pro.local>
2023-02-03 23:15:44 +0530

    Added super admins list and added override permission logic for editing users

--- src/components/addEditUserForm.js
+++ src/components/addEditUserForm.js
@@ -12,14 +12,16 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { countries, USA_STATES, CANADA_PROVINCES, asthaans, HerokuURL, AirlineNames, INDIA_AIRPORT_LIST, LOCAL_DATE_FORMAT } from '../constants';
+import { countries, USA_STATES, CANADA_PROVINCES, asthaans, HerokuURL, AirlineNames, INDIA_AIRPORT_LIST, LOCAL_DATE_FORMAT, OVERIDE_USER_LIST } from '../constants';
 import AutoCompleteWithOther from './helper/autoCompleteWithOther'
+
 const AddEditUser = (props) => {
   const { user, handleCloseCallback, currentUser, onSaveSuccessCallBack } = props;
 
 
   const [showArrivingWithinThreeDays, setShowArrivingWithinThreeDays] = useState(false);
   const [stateOptions, setStateOptions] = useState(null);
+  const [overrideValidation, setOverrideValidation] = useState(OVERIDE_USER_LIST.includes(currentUser) ? true : false);
   const [showValidationMessages, setShowValidationMessages] = useState(false);
   const [loading, setLoading] = useState(false);
   const [snack, setSnack] = useState({ message: "", duration: 0, open: false, severity: "success" })
@@ -64,7 +66,7 @@ const AddEditUser = (props) => {
   }
 
   const isValidRequiredField = (fieldValue) => {
-    if (showValidationMessages && !fieldValue) {
+    if (showValidationMessages && !fieldValue && overrideValidation) {
       return false;
     }
     return true;
@@ -149,6 +151,7 @@ const AddEditUser = (props) => {
   }
 
   const isValidTime = (timeValue, isRequired) => {
+    if (overrideValidation) return true;
     if (!showValidationMessages) {
       return true;
     }
@@ -161,6 +164,9 @@ const AddEditUser = (props) => {
   }
 
   const isValidDate = (dateValue, isRequired) => {
+    if (overrideValidation) {
+      return true;
+    }
     if (!showValidationMessages) {
       return true;
     }
@@ -176,6 +182,8 @@ const AddEditUser = (props) => {
   const isValidForm = () => {
 
     // required fields
+
+    if (overrideValidation) return true;
 
     let isValid = true;
     if (!sangatValue.user_firstName || !sangatValue.user_lastName || !sangatValue.user_middleName || !sangatValue.user_gender || !sangatValue.user_yearOfBirth || !sangatValue.user_goingToAsthan || !sangatValue.user_country || !sangatValue.user_state || !sangatValue.user_city || !sangatValue.user_phoneNumber || !sangatValue.user_arrivingFlightName || !sangatValue.user_arrivingFlightNumber || !sangatValue.user_arrivingFlightDate || !sangatValue.user_arrivingFlightTime || !sangatValue.user_arrivingFlightAirport || !sangatValue.user_ride_from_airport || !sangatValue.user_departingFlightName || !sangatValue.user_departingFlightNumber || !sangatValue.user_departingFlightDate || !sangatValue.user_departingFlightTime || !sangatValue.user_departingFlightAirport) {
@@ -206,7 +214,7 @@ const AddEditUser = (props) => {
     if (!isValidForm()) {
       return;
     }
-    if (Math.abs(dayjs().diff(sangatValue.user_arrivingFlightDate, 'day')) <= 3 && currentUser !== 'sandeep') {
+    if (Math.abs(dayjs().diff(sangatValue.user_arrivingFlightDate, 'day')) <= 3 && overrideValidation) {
       setShowArrivingWithinThreeDays(true);
     }
     else {
@@ -214,11 +222,15 @@ const AddEditUser = (props) => {
       let dobLocalDateFormat = sangatValue.user_yearOfBirth.format(LOCAL_DATE_FORMAT);
 
       {/* TODO: front end and backend name mismatch */ }
-      let sangatValueToSave = {
-        ...sangatValue, user_yearOfBirth: dobLocalDateFormat, user_arrivingFlightDate: sangatValue.user_arrivingFlightDate.format(LOCAL_DATE_FORMAT),
-        user_arrivingFlightTime: sangatValue.user_arrivingFlightTime.format("HH:mm"), user_departingFlightDate: sangatValue.user_departingFlightDate.format(LOCAL_DATE_FORMAT),
-        user_departingFlightTime: sangatValue.user_departingFlightTime.format("HH:mm")
-      };
+      let sangatValueToSave = {};
+      if (!overrideValidation) {
+        sangatValueToSave = {
+          ...sangatValue, user_yearOfBirth: dobLocalDateFormat, user_arrivingFlightDate: sangatValue.user_arrivingFlightDate.format(LOCAL_DATE_FORMAT),
+          user_arrivingFlightTime: sangatValue.user_arrivingFlightTime.format("HH:mm"), user_departingFlightDate: sangatValue.user_departingFlightDate.format(LOCAL_DATE_FORMAT),
+          user_departingFlightTime: sangatValue.user_departingFlightTime.format("HH:mm")
+        };
+      }
+
       setLoading(true);
       let saveUserResponse;
       if (sangatValue && sangatValue._id) {
@@ -230,7 +242,7 @@ const AddEditUser = (props) => {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify(
-            sangatValueToSave
+            overrideValidation ? sangatValue : sangatValueToSave
           ),
         })
       } else {
@@ -255,11 +267,13 @@ const AddEditUser = (props) => {
         setSnack({ open: true, severity: "success", durration: 6000, message: "This user was successfully saved." })
         setLoading(false);
 
-        setSangatValue({
-          ...data, user_yearOfBirth: dayjs(data.user_yearOfBirth, LOCAL_DATE_FORMAT), user_arrivingFlightDate: dayjs(data.user_arrivingFlightDate, LOCAL_DATE_FORMAT),
-          user_arrivingFlightTime: dayjs(data.user_arrivingFlightTime, "HH:mm"), user_departingFlightDate: dayjs(data.user_departingFlightDate, LOCAL_DATE_FORMAT),
-          user_departingFlightTime: dayjs(data.user_departingFlightTime, "HH:mm")
-        });
+        if (!overrideValidation) {
+          setSangatValue({
+            ...data, user_yearOfBirth: dayjs(data.user_yearOfBirth, LOCAL_DATE_FORMAT), user_arrivingFlightDate: dayjs(data.user_arrivingFlightDate, LOCAL_DATE_FORMAT),
+            user_arrivingFlightTime: dayjs(data.user_arrivingFlightTime, "HH:mm"), user_departingFlightDate: dayjs(data.user_departingFlightDate, LOCAL_DATE_FORMAT),
+            user_departingFlightTime: dayjs(data.user_departingFlightTime, "HH:mm")
+          });
+        }
 
         if (Math.abs(dayjs().diff(sangatValue.user_arrivingFlightDate, 'day')) <= 3) {
           setShowArrivingWithinThreeDays(true);
@@ -383,6 +397,7 @@ const AddEditUser = (props) => {
   const setUserRideFromAirport = (e) => {
     setSangatValue({ ...sangatValue, user_ride_from_airport: e.target.value ? e.target.value.toUpperCase() : "" })
   }
+
 
   return (
     <Dialog maxWidth={"md"} open={true} onClose={handleClose} >
